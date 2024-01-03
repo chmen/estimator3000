a0e2200c696e370b5878b6ffbf391482b74e390c
chmen <tony.potapov@gmail.com>
2024-01-03 18:18:56 +0200

    add raw api call to estimats endpoint

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,14 @@
 <template>
   <div>
     <h1>Estimate</h1>
+
+    <p>{{ count }}</p>
     <AppAlert>
       It is a good day to die
     </AppAlert>
   </div>
 </template>
+
+<script setup lang="ts">
+  const { data: count } = await useFetch('http://localhost:3000/api/v1/estimates')
+</script>
