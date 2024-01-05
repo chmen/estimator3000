60baa1bfb90012c142c0557e63ca2fd7700d316b
chmen <tony.potapov@gmail.com>
2024-01-05 16:01:31 +0200

    add list rendering

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
-    <h1>Estimate</h1>
+    <h1>Estimates</h1>
 
     <p>{{ count }}</p>
-    <AppAlert>
-      It is a good day to die
-    </AppAlert>
+    <div v-for="estimate in estimates">
+      {{ estimate.id }} {{ estimate.title }}
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-  const { data: count } = await useFetch('http://localhost:3000/api/v1/estimates')
+  const { data: estimates } = await useFetch('http://localhost:3000/api/v1/estimates')
 </script>
