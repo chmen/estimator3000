62f7cb64a93e072d44b4b4393a89d9e9eef858ea
chmen <tony.potapov@gmail.com>
2024-01-05 16:28:22 +0200

    add show page for estimate

---
+++ pages/estimates/[id].vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>{{ estimate.id }} {{ estimate.title }}</h1>
+
+    <p>{{ estimate.body }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+  const { data: estimate } = await useFetch('http://localhost:3000/api/v1/estimates/1')
+</script>
